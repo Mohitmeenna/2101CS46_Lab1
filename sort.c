064d736a4f6b6f289ce0f04aa52be68a548b6468
Mohitmeenna <110120435+Mohitmeenna@users.noreply.github.com>
2022-07-27 23:16:03 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,3 +1,36 @@
+#include <stdio.h>
+void print_array(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void InsertionSort(int array[], int size)
+{
+    for (int step = 1; step < size; step++)
+    {
+        int key = array[step];
+        int j = step - 1;
+
+        while (key < array[j] && j >= 0)
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
+
+
 int main()
 {
 
