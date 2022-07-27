bdb34747628a357216252456c6cbe928fe332c9e
Mohitmeenna <110120435+Mohitmeenna@users.noreply.github.com>
2022-07-27 23:31:32 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -63,6 +63,89 @@ void BubbleSort(int array[], int size)
         }
     }
 }
+void Merge(int arr[], int p, int q, int r)
+{
+
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
+void MergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+
+        int m = l + (r - l) / 2;
+
+        MergeSort(arr, l, m);
+        MergeSort(arr, m + 1, r);
+
+        Merge(arr, l, m, r);
+    }
+}
+int Partition(int array[], int low, int high)
+{
+
+    int pivot = array[high];
+
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+
+            i++;
+
+            swap(&array[i], &array[j]);
+        }
+    }
+
+    swap(&array[i + 1], &array[high]);
+
+    return (i + 1);
+}
 
 int main()
 {
@@ -96,16 +179,25 @@ int main()
         InsertionSort(data, size);
         printf("THIS IS THE SORTED ARRAY IN ASCENDING ORDER:\n");
         print_array(data, size);
-    } else if (op == 2)
+    }
+    else if (op == 2)
     {
         SelectionSort(data, size);
         printf("THIS IS THE SORTED ARRAY IN ASCENDING ORDER:\n");
         print_array(data, size);
-    }   else if (op == 3)
+    }
+    else if (op == 3)
     {
         BubbleSort(data, size);
         printf("THIS IS THE SORTED ARRAY IN ASCENDING ORDER:\n");
         print_array(data, size);
     }
-  return 0;
+    else if (op == 4)
+    {
+        MergeSort(data, 0, size - 1);
+        printf("THIS IS THE SORTED ARRAY IN ASCENDING ORDER: \n");
+        print_array(data, size);
+    }
+  
+    return 0;
 }
