576f3c9da9f55d17f26e0dc70d6999d9514b75f2
Mohitmeenna <110120435+Mohitmeenna@users.noreply.github.com>
2022-07-27 22:20:28 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// function for addition
+void addition(long int a, long int b)
+{
+    printf("RESULT = %ld", a + b);
+}
+int main()
+{
+    long int k;
+    long int l;
+    printf("Enter num1:\n");
+    scanf("%ld", &k);
+    printf("Enter num2:\n");
+
+    scanf("%ld", &l);
+    printf("Enter code of the operation needed:\n 1. Addition \t 2. Subtract \t 3. Multiply \t 4. Divide\n");
+    int n;
+    scanf("%d", &n);
+    switch (n)
+    {
+    case 1:
+        addition(k, l);
+        break;
+    }
+    return 0;
+}
